73ffcb1678d98cde7c86f33bd4185537f1abc04e
PHM-dung2 <pp4992@naver.com>
2025-04-04 12:01:55 +0900

    day02 flutter 조건문 예제

---
+++ lib/example/day02/example3.dart
@@ -0,0 +1,114 @@
+void main(){
+
+  // 조건문
+  // int number = 31;
+  // if( number % 2 == 1 ) {
+  //   print("홀수!");
+  // }else{
+  //   print("짝수!");
+  // } // if end
+
+  // 복잡한 조건문
+  // String light = "green";
+  // if( light == "green" ){
+  //   print("초록불!");
+  // }else if( light == "yellow" ){
+  //   print("노란불!");
+  // }else if( light == "red" ){
+  //   print("빨간불!");
+  // }else{
+  //   print("잘못된 신호입니다.");
+  // }
+
+  // String light = "purple";
+  // if( light == "green" ){
+  //   print("초록불!");
+  // }else if( light == "yellow" ){
+  //   print("노란불!");
+  // }else if( light == "red" ){
+  //   print("빨간불!");
+  // }
+
+  // 반복문
+  // for( int i = 0 ; i < 100 ; i++ ){
+  //   print( i+1 );
+  // } // for end
+
+  // JAVA 향상된 for문과 비슷
+  // List<String> subjects = ["자료구조", "이산수학", "알고리즘", "플러터"];
+  // for( String subject in subjects ){
+  //   print(subject);
+  // }
+
+  // while
+  // int i = 0;
+  // while( i < 100 ){
+  //   print( i+1 );
+  //   i = i + 1;
+  // }
+
+  // 무한 루프
+  // int i = 0;
+  // while( true ){
+  //   print( i+1 );
+  //   i = i + 1;
+  //   }
+  // }
+
+  // 무한루프 - break
+  // int i = 0;
+  // while( true ){
+  //   print( i+1 );
+  //   i = i + 1;
+  //   if( i == 100 ){
+  //     break;
+  //   }
+  // }
+
+  // 짝수 출력
+  // for( int i = 0; i < 100; i++ ){
+  //   if( i % 2 == 0 ){
+  //     continue;
+  //   }
+  //   print( i+1 );
+  // }
+
+  // 함수
+  // int number = add( 1, 2 );
+  // print( number );
+
+  // switch
+  // int number = 1;
+  // switch( number ){
+  //   case 1 :
+  //     print('one');
+  // }
+
+  // const a = 'a';
+  // const b = 'b';
+  // const obj = [ a, b ];
+  // switch( obj ){
+  //   case [ a, b ] :
+  //     print('$a, $b');
+  // }
+
+  // dynamic obj = 1;
+  // var obj = ( 'a' , 2 );
+  // switch( obj ){
+  //   case 1:
+  //     print('one');
+  //   // case >= 2 && <= 4 :
+  //   //   print('in range');
+  //   case ( var a, var b ) :
+  //     print( 'a = $a, b = $b' );
+  //
+  //     default :
+  // }
+
+
+} // f end
+
+// 함수
+int add( int a, int b){
+  return a + b;
+}
